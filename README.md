613888fc940c4e4fc31929472c8eb3b1f77fc262
Shaun Newman <shaun.newman.p@gmail.com>
2025-01-16 13:36:19 -0500

    moved psudocode around and added readme

---
+++ README.md
@@ -0,0 +1,8 @@
+First JS project on Fullstack Academy
+# Goals
+* Create varibles
+* Proper use of operators 
+* Use template literals
+* Psudocode
+* Use console.log and alert
+* Using git and GitHub
---
+++ index.js
@@ -0,0 +1,14 @@
+// create the first alert
+alert(`You have received this message because you have been chosen to open an important vault. Here is the secret combination:`);
+
+// create varible 1 add operator to variable 1
+const numberOne = 2 + 8;
+
+// create varible 2 add operator to variable 2
+const numberTwo = 120 / 3;
+
+// create varible 3 add operator to variable 3
+const numberThree = 51 - 12;
+
+// alert the number with the three varibles using template literals
+alert(`${numberOne} - ${numberTwo} - ${numberThree}`);
